131c486c30b77fe5fb6d2dacee00e1ce6c58fc24
Laura Beaufort <lbeaufort@fec.gov>
2022-05-03 12:25:54 -0400

    Add UUID to extended user model

--- mysite/simpleapp/admin.py
+++ mysite/simpleapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from simpleapp.models import FECFileUser
+
+# Define an inline admin descriptor for FECFileUserInline model
+# which acts a bit like a singleton
+# This allows us to see FECFile user fields in app admin
+class FECFileUserInline(admin.StackedInline):
+    model = FECFileUser
+    can_delete = False
+    verbose_name_plural = 'FECFileUser'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (FECFileUserInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- mysite/simpleapp/models.py
+++ mysite/simpleapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FECFileUser(models.Model):
+    """
+    Add UUID from login.gov to default User model
+    https://developers.login.gov/attributes/
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    uuid = models.CharField(max_length=36)
+    # TODO: may want `all_emails`, `phone`
