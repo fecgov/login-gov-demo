1f34c65ea7d1533b0c3c418c904161208df266b1
Laura Beaufort <lbeaufort@fec.gov>
2022-05-04 12:54:49 -0400

    Try to fix staticfiles

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@ cd mysite
 # Run migrations
 ./manage.py migrate --noinput
 
-gunicorn --bind 0.0.0.0:8080 mysite.wsgi -w 9 -t 200
+python manage.py collectstatic --noinput && gunicorn --bind 0.0.0.0:8080 mysite.wsgi -w 9 -t 200
