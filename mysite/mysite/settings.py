5f8a8633e30c3cf95539473f1134fe513e1a8d3d
Laura Beaufort <lbeaufort@fec.gov>
2022-05-04 13:11:42 -0400

    Add static root

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -18,6 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
