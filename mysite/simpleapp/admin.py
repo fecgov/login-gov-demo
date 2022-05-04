16caa18d23319bad9a0d96186780bcb70e923a42
Laura Beaufort <lbeaufort@fec.gov>
2022-05-04 12:17:31 -0400

    Add UUID to admin interface

--- mysite/simpleapp/admin.py
+++ mysite/simpleapp/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            'Login.gov information',  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'uuid',
+                ),
+            },
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
