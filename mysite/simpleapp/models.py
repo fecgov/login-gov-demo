1e1c570362c549958eef81e9c98fb43a711d2fcf
Laura Beaufort <lbeaufort@fec.gov>
2022-05-04 14:13:32 -0400

    Allow first and last names to be blank

--- mysite/simpleapp/models.py
+++ mysite/simpleapp/models.py
@@ -9,3 +9,6 @@ class User(AbstractUser):
     """
     uuid = models.CharField(max_length=36)
     # TODO: may want `all_emails`, `phone`
+    # Allow first and last names to be blank
+    first_name= models.CharField(null=True, blank=True, max_length=100)
+    last_name = models.CharField(null=True, blank=True, max_length=100)
